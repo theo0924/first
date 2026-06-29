ef03d34aa1fc9a4f21455a0a6af687605fad994f
theo0924 <Sgw01924@icloud.com>
2026-06-29 14:38:25 +0800

    second

--- 1.c
+++ 1.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
+#include <float.h>
  
 int main()
 {
-    /* 我的第一个 C 程序 */
-    printf("Hello, World! \n");
- 
-    return 0;
+   printf("float 存储字节数 : %zu \n", sizeof(float));
+   printf("float 最小值: %E\n", FLT_MIN );
+   printf("float 最大值: %E\n", FLT_MAX );
+   printf("精度值: %d\n", FLT_DIG );
+   
+   return 0;
 }
--- 2.c
+++ 2.c
@@ -3,7 +3,7 @@
  
 int main()
 {
-   printf("float 存储最大字节数 : %lu \n", sizeof(float));
+   printf("float 存储字节数 : %zu \n", sizeof(float));
    printf("float 最小值: %E\n", FLT_MIN );
    printf("float 最大值: %E\n", FLT_MAX );
    printf("精度值: %d\n", FLT_DIG );
